0730a6d2bb1a79db765f6ee727e3e9be96dd6ebc
Lusin-mus <lusnarmus@gmail.com>
2019-12-15 17:42:43 +0400

    First commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,13 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	int [] array={1, 2, 4, 5, 5};
+	int sum=0;
+	for(int i=0; i< array.length; i++){
+	    sum+=array[i];
+    }
+	System.out.println(sum);
+    }
+}
