3bcd00f5837129e6f2bef116b9437a2278d432ab
Lusin-mus <lusnarmus@gmail.com>
2019-12-15 17:48:27 +0400

    Add some numbers

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,7 +3,7 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	int [] array={1, 2, 4, 5, 5};
+	int [] array={1, 2, 4, 5, 5, 7, 9};
 	int sum=0;
 	for(int i=0; i< array.length; i++){
 	    sum+=array[i];
