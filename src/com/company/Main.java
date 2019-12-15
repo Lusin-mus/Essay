d6cb6b3866b340218ddf20ee729a3cf0667e4a6b
Lusin-mus <lusnarmus@gmail.com>
2019-12-15 19:10:48 +0400

    third commit

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,7 +3,7 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	int [] array={1, 2, 4, 5, 5};
+	int [] array={1, 2, 4, 5, 5, 7, 3, 5, 0};
 	int sum=0;
 	for(int i=0; i< array.length; i++){
 	    sum+=array[i];
